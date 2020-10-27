3e2c3ca7a2a99915c9c1bb39df806db8cc5d5986
dobl94 <62128400+dobl94@users.noreply.github.com>
2020-10-27 14:01:46 +0900

    Update transfer.js

--- HCN_Beta/transfer.js
+++ HCN_Beta/transfer.js
@@ -1,4 +1,4 @@
-	var gameinstance = UnityLoader.instantiate("unityContainer", "Build/HCN_Beta.json", {onProgress: UnityProgress});
+	var gameInstance = UnityLoader.instantiate("unityContainer", "Build/HCN_Beta.json", {onProgress: UnityProgress});
 	// More API functions here:
    // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/pose
 
